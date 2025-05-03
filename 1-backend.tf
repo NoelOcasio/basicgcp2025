973770249621ecc465daf14250490503624066f5
Darth Bane 2025 <balericaclasssixfive@gmail.com>
2025-05-03 12:41:40 -0500

    Create 1-backend.tf

---
+++ 1-backend.tf
@@ -0,0 +1,15 @@
+# https://www.terraform.io/language/settings/backends/gcs
+terraform {
+  backend "gcs" {
+    bucket = "lizzobootybucket"
+    prefix = "terraform/state"
+    credentials = "yourcreds.json"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
