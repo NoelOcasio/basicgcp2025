fd93c7bb5383fc085f345b620c5d794e048d1cb4
NoelOcasio <ocasio889@gamail.com>
2025-05-13 20:13:47 -0500

    Edits

--- 0-authentication.tf
+++ 0-authentication.tf
@@ -1,11 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 provider "google" {
-  project = "yourproject"
-  region  = "lizzobootycheek"
+  project = "Terraform Project"
+  region  = ""
   credentials = "yourkey.json"
 }
 
-# I'm doing this tonight while prepping my students for their sessions with Lizzo
-
-# comment there will be no diddy oil bootygasms here
-# Lizzo will be pegging Carlton and inducing a bootygasm deep in him with Chewbacca watching!
